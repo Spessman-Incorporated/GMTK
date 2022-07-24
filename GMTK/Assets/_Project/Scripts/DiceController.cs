1aa8e6403da3786834d3d14846fc6c14db1b6f70
Victor <ss.victorguilherme@gmail.com>
2022-07-24 13:44:48 -0300

    colocando dado ao apertar espaço

---
+++ GMTK/Assets/_Project/Scripts/DiceController.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Coimbra;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using Random = System.Random;
+
+public class DiceController : MonoBehaviour
+{
+    public SpriteRenderer DiceSprite;
+    public List<Sprite> DiceNumbers =  new List<Sprite>();
+    
+    private void Awake()
+    {
+        RollTheDice();
+    }
+    
+    private async void RollTheDice()
+    {
+        PlayerController.CanPlaceDice = false;
+        
+        Random rng = new Random();
+        List<Sprite> spriteList = new List<Sprite>(DiceNumbers.OrderBy(item => rng.Next()));
+
+        foreach (Sprite sprite in spriteList)
+        {
+            DiceSprite.sprite = sprite;
+            await WaitForSeconds(0.5f);
+        }
+
+        PlayerController.CanPlaceDice = true;
+        gameObject.Destroy();
+    }
+
+    private async UniTask WaitForSeconds(float seconds)
+    {
+        await UniTask.Delay(TimeSpan.FromSeconds(seconds));
+    }
+}
