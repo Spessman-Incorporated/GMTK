ab488a73a4ea6542e244fcd776904cd238670b6c
Victor <ss.victorguilherme@gmail.com>
2022-07-24 15:55:04 -0300

    Testando explosoes e colisores

--- GMTK/Assets/_Project/Scripts/DiceController.cs
+++ GMTK/Assets/_Project/Scripts/DiceController.cs
@@ -11,6 +11,11 @@ public class DiceController : MonoBehaviour
     public SpriteRenderer DiceSprite;
     public List<Sprite> DiceNumbers =  new List<Sprite>();
     public float ChangeSideTime;
+
+    public List<GameObject> UpExplosions = new List<GameObject>();
+    public List<GameObject> DownExplosions = new List<GameObject>();
+    public List<GameObject> LeftExplosions = new List<GameObject>();
+    public List<GameObject> RightExplosions = new List<GameObject>();
     
     private void Awake()
     {
@@ -31,6 +36,36 @@ public class DiceController : MonoBehaviour
         }
 
         PlayerController.CanPlaceDice = true;
+
+        ExplodeCells();
+    }
+
+    public async void ExplodeCells()
+    {
+        DiceSprite.gameObject.SetActive(false);
+
+        foreach (GameObject explostion in UpExplosions)
+        {
+            explostion.GetComponent<DiceExplosionController>().Explode();
+        }
+        
+        foreach (GameObject explostion in DownExplosions)
+        {
+            explostion.GetComponent<DiceExplosionController>().Explode();
+        }
+        
+        foreach (GameObject explostion in LeftExplosions)
+        {
+            explostion.GetComponent<DiceExplosionController>().Explode();
+        }
+        
+        foreach (GameObject explostion in RightExplosions)
+        {
+            explostion.GetComponent<DiceExplosionController>().Explode();
+        }
+        
+        await WaitForSeconds(0.5f);
+
         gameObject.Destroy();
     }
 
